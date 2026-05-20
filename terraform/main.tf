20960d4a125daa1c1292cfab5377a27d1571123e
nameismain <idid02@naver.com>
2026-05-20 23:07:30 +0900

    Rebuild project structure for cloud threat detection lab

--- terraform/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.10.0.0/16"
-
-  tags = {
-    Name    = "${var.project_name}-vpc"
-    Project = var.project_name
-  }
-}
--- terraform/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "vpc_id" {
-  description = "Created VPC ID"
-  value       = aws_vpc.main.id
-}
